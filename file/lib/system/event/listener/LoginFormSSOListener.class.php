47abd70fa2a02fdfd2c5c152f6f4fe4457655478
Tim Düsterhus <timwolla@arcor.de>
2013-01-16 19:43:46 +0100

    Add eventListener barebone

---
+++ file/lib/system/event/listener/LoginFormSSOListener.class.php
@@ -0,0 +1,20 @@
+<?php
+namespace wcf\system\event\listener;
+
+/**
+ * Performs SSO after login.
+ *
+ * @author 	Maximilian Mader
+ * @copyright	2010-2012 Tim Düsterhus
+ * @license	Creative Commons Attribution-NonCommercial-ShareAlike <http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode>
+ * @package	be.bastelstu.wcf.chat
+ * @subpackage	system.event.listener
+ */
+class ChatRouteListener implements \wcf\system\event\IEventListener {
+	/**
+	 * @see	\wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		
+	}
+}
